c69e1769a4d9ccb535c1b84f7b31035771825fec
tunalad <novica.arsic@aol.com>
2022-06-10 20:01:03 +0200

    Wrote an API handler

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from .utils import accuweather_handling as awh
 
 # Create your views here.
 from weather_app.models import Place
 
+API_KEY = "8FPhfrAB4eSqEUqWGxOccCRWPFbqDePu"
 
 def index(request):
     return render(request, 'index.html')
@@ -12,7 +14,11 @@ def index(request):
 def nav_places(request):
     p = Place.objects.all()
     dumms = dummy_items()
-    return render(request, 'index.html', {'places': p, 'dummies': dumms})
+    place_key = awh.get_key(API_KEY, "Belgrade")
+
+    return render(request, 'index.html', {'places': p,
+                                          'dummies': dumms,
+                                          'place_key': place_key})
 
 
 def dummy_items():
